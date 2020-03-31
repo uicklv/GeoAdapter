d1b2b3616d592915e87d961fa63a6abd9a99a83a
uicklv <uicklv@gmail.com>
2020-03-31 22:37:27 +0300

    finish

---
+++ src/Maxmind.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Lib;
+
+
+use GeoIp2\Database\Reader;
+use GeoIp2\Exception\AddressNotFoundException;
+
+class MaxmindAdapter
+{
+
+    protected  $reader;
+    protected  $record;
+
+    public function __construct(Reader $reader)
+    {
+        $this->reader = $reader;
+    }
+
+    public function parse(string $ip)
+    {
+        try {
+            $this->record = $this->reader->city(request()->ip());
+        } catch (AddressNotFoundException $exception) {
+            $this->record = $this->reader->city(env('DEFAULT_IP_ADDR'));
+        }
+    }
+
+
+    public function getCountryCode()
+    {
+        return $this->record->country->isoCode;
+    }
+
+    public function getCityName()
+    {
+        return $this->record->city->name;
+    }
+}
